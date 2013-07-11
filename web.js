4b9b46a654d48456bd84ba8728f17d3da41b8e83
Ben Wagley <benjamin.wagley@gmail.com>
2013-07-11 23:56:24 +0000

    Added file

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var buffer = new Buffer('utf-8');
-var data = fs.readFileSync('index.html', 'utf-8', buffer);
+var data = fs.readFileSync('index.html',  buffer);
 var html = buffer.toString();
 
 var app = express.createServer(express.logger());
