a5ccceca78ce42c043052e671c539fba6d6779b4
Ben Wagley <benjamin.wagley@gmail.com>
2013-07-11 22:21:35 +0000

    Added file

--- web.js
+++ web.js
@@ -2,11 +2,12 @@ var express = require('express');
 var fs = require('fs');
 var buffer = new Buffer(" ");
 var data = fs.readFileSync('index.html', 'utf-8', buffer, 0, 0, 1);
+var html = buf.toString(buffer);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer);
+  response.send(html);
 });
 
 var port = process.env.PORT || 5000;
